f600d7baff3fa2ae0d184427163c5baf5fc802ee
Virendra <virendra.limbad@azilen.com>
2020-08-27 14:23:28 +0530

    new files added

---
+++ sample-rest/src/main/java/com/sample/rest/SampleRestApplication.java
@@ -0,0 +1,13 @@
+package com.sample.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApplication.class, args);
+	}
+
+}
---
+++ sample-rest/src/main/java/com/sample/rest/controller/RestController.java
@@ -0,0 +1,19 @@
+package com.sample.rest.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+}
